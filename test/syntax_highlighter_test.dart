3d3ef2812d84ded3e91e13d0f0195c3986b0983c
Jonathan Gao <gsmlg.com@gmail.com>
2022-04-05 02:43:19 +0800

    fix: Add test.

--- test/syntax_highlighter_test.dart
+++ test/syntax_highlighter_test.dart
@@ -1,3 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:markdown_syntax_highlighter/syntax_highlighter.dart';
 
-void main() {}
+void main() {
+  test('iOS icon list is correct size', () {
+    final String md = '''
+    import 'dart:io';
+    ''';
+    final hl = DartSyntaxHighlighter();
+    expect(hl.format(md).runtimeType, TextSpan().runtimeType);
+  });
+}
